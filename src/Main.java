97d0ddff371e18d0697282a61674499b618bc67b
David Mortensen <davidmortensen90@gmail.com>
2023-01-23 16:27:16 +0100

    printed the price of book object to the console

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Book bok1 = new Book("DJ Khaled", 999);
         System.out.println(bok1.name);
+        System.out.println(bok1.price);
     }
 }
