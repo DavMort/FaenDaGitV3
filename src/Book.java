ee4de01a4fc3de7615cfb42549602b76729c05f1
David Mortensen <davidmortensen90@gmail.com>
2023-01-23 16:40:15 +0100

    Added top-selling metric to the book class

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,11 @@
 public class Book {
     String name;
     int price;
+    boolean topSeller;
 
-    public Book(String name, int price) {
+    public Book(String name, int price, boolean topSeller) {
         this.name = name;
         this.price = price;
+        this.topSeller = topSeller;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Book bok1 = new Book("DJ Khaled", 999);
+        Book bok1 = new Book("DJ Khaled", 999, true);
         System.out.println(bok1.name);
         System.out.println("The book printed above costs $" + bok1.price);
+        System.out.println(bok1.topSeller ? "This is a top selling book!" : "This is not a very popular book");
     }
 }
